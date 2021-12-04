516e67937c70f4297e377ff1cd9be675f8550361
Samuel Rey <samuel.rey.escudero@gmail.com>
2021-12-04 12:47:30 -0600

    improving initialization

--- spectral_nti.py
+++ spectral_nti.py
@@ -1,6 +1,5 @@
 import cvxpy as cp
 import numpy as np
-from numpy import linalg
 
 import utils
 
@@ -11,27 +10,53 @@ def SGL(C, g_funcs, cs, regs, max_iters=100, epsilon=1e-6, verbose=False):
     return SGL_MM(C, g_funcs, [], cs, regs_aux, max_iters, epsilon, verbose)
 
 
-def SGL_MM(C, g_funcs, up_bounds, cs, regs, max_iters=100, epsilon=1e-6,
-           verbose=False, verb_solv=False):
+def step3(d, lambdas, g_funcs, up_bounds, cs, rs):
+    # Step 3: solved with cvx
+    N = lambdas.shape[0]
+    lambdas_hat = cp.Variable(N-1)  # Fisrt eigenvalue is 0
+    contraints = []
+    for i, g_func in enumerate(g_funcs):
+        expr = g_func(lambdas_hat, N)
+        if expr.curvature == "AFFINE":
+            contraints.append((expr - cs[i])**2 <= rs['deltas'][i]**2)
+        elif expr.curvature == "CONCAVE":
+            contraints.append((expr - cs[i]) >= -rs['deltas'][i])
+        else:
+            contraints.append((expr - cs[i]) <= rs['deltas'][i])
+
+    up_bounds_obj = 0
+    for up_bound in up_bounds:
+        up_bounds_obj += rs['gamma']*up_bound(lambdas_hat, lambdas[1:], N)
+
+    obj = cp.Minimize(cp.sum(-cp.log(lambdas_hat)) \
+         + rs['beta']/2*cp.sum_squares(lambdas_hat-d) + up_bounds_obj)
+
+    prob = cp.Problem(obj, contraints)        
+    prob.solve()
+    lambdas = np.concatenate(([0], lambdas_hat.value))
+    return lambdas, prob.status
+
+
+def SGL_MM(C, g_funcs, up_bounds, cs, regs, max_iters=100, epsilon=1e-4,
+           verbose=False):
     N = C.shape[0]
-    alpha = regs['alpha']
-    beta = regs['beta']
-    gamma = regs['gamma']
-    deltas = regs['deltas']
-    
+
     # Check inputs
     if not isinstance(g_funcs, list):
         g_funcs = [g_funcs]
     if not isinstance(cs, list) and np.isscalar(cs):
         cs = [cs]
-    if not isinstance(deltas, list) and np.isscalar(deltas):
-        deltas = [deltas]
+    if not isinstance(regs['deltas'], list) and np.isscalar(regs['deltas']):
+        regs['deltas'] = [regs['deltas']]
     if not isinstance(up_bounds, list):
         up_bounds = [up_bounds]
-
-    K = C + alpha*(2*np.eye(N)-np.ones((N,N)))
+    
+    K = C + regs['alpha']*(2*np.eye(N)-np.ones((N,N)))
 
     L = np.linalg.pinv(C, rcond=1e-6, hermitian=True)
+    L = np.where(L > 0, 0, L)
+    L[np.eye(N, dtype=bool)] = -1*np.sum(L,1)
+
     w = utils.L_inv_op(L)
     lambdas, V = np.linalg.eigh(L)
     prev_L = L
@@ -40,50 +65,34 @@ def SGL_MM(C, g_funcs, up_bounds, cs, regs, max_iters=100, epsilon=1e-6,
     # prev_opt_val = 1e6
     for t in range(max_iters):
         # Step 1: closed form solution
-        z = utils.Lstar_op(V@np.diag(lambdas)@V.T-1/beta*K)
+        z = utils.Lstar_op(V@np.diag(lambdas)@V.T-1/regs['beta']*K)
         gradient = utils.Lstar_op(utils.L_op(w))-z
         w = np.maximum(0, w-gradient/(2*N))
         L = utils.L_op(w)
+        
+        if np.all((w == 0)):
+            print('WARNING: L_hat is 0. Returning L from previous iteration')
+            return prev_L, prev_lam
 
         # Step 2: eigendecomposition of step 1
         _, V = np.linalg.eigh(L)
 
-        # Step 3: solved with cvx
         d = np.diag(V.T@L@V)[1:]
-        lambdas_hat = cp.Variable(N-1)  # Fisrt eigenvalue is 0
-        contraints = []
-        for i, g_func in enumerate(g_funcs):
-            expr = g_func(lambdas_hat, N)
-            if expr.curvature == "AFFINE":
-                contraints.append((expr - cs[i])**2 <= deltas[i])
-            elif expr.curvature == "CONCAVE":
-                contraints.append((expr - cs[i]) >= -deltas[i])
-            else:
-                contraints.append((expr - cs[i]) <= deltas[i])
-
-        up_bounds_obj = 0
-        for up_bound in up_bounds:
-            up_bounds_obj += gamma*up_bound(lambdas_hat, lambdas[1:], N)
-
-
-        obj = cp.Minimize(cp.sum(-cp.log(lambdas_hat)) \
-            + beta/2*cp.sum_squares(lambdas_hat-d) + up_bounds_obj)
-
-        prob = cp.Problem(obj, contraints)        
-        prob.solve(verbose=verb_solv)
-        lambdas = np.insert(lambdas_hat.value,0,0)  # Prepend eigenvalue 0 at the begining
+        lambdas, prob_status = step3(d, lambdas, g_funcs, up_bounds, cs, regs)
         
-        if np.all((L == 0)):
-            print('WARNING: L_hat is 0. Returning L from previous iteration')
-            return prev_L, lambdas
+        if prob_status in ['infeasible', 'unbounded', 'infeasible_inaccurate']:
+            print('WARNING: problem status', prob_status)
+            return prev_L, prev_lam
 
-        L_conv = np.linalg.norm(L-prev_L, 'fro')**2/np.linalg.norm(prev_L, 'fro')**2
+        L_conv = np.linalg.norm(L-prev_L, 'fro')/np.linalg.norm(prev_L, 'fro')
         lambd_conv = np.linalg.norm(lambdas-prev_lam, 2)**2/np.linalg.norm(prev_lam, 2)**2
+        prev_lam = lambdas
         prev_L = L
+
         if verbose:
             sim_Ls = np.linalg.norm(L-V@np.diag(lambdas)@V.T,'fro')
             print('{}. status: {} - L_cnv: {:.6f} - Lam_cnv: {:.3f} - L-VLambdaL\': {:.3f}'.
-                format(t, prob.status, L_conv, lambd_conv, sim_Ls))
+                format(t, prob_status, L_conv, lambd_conv, sim_Ls))
             
             for i, g_func in enumerate(g_funcs):
                 c_aux =  g_func(lambdas, N).value
@@ -139,23 +148,23 @@ def SGL_show_conv(C, g_funcs, up_bounds, cs, regs, A_true,
                   max_iters=500, epsilon=1e-6,
                   verbose=True):
     N = C.shape[0]
-    alpha = regs['alpha']
-    beta = regs['beta']
-    gamma = regs['gamma']
-    deltas = regs['deltas']
-    
+
     # Check inputs
     if not isinstance(g_funcs, list):
         g_funcs = [g_funcs]
     if not isinstance(cs, list) and np.isscalar(cs):
         cs = [cs]
-    if not isinstance(deltas, list) and np.isscalar(deltas):
-        deltas = [deltas]
+    if not isinstance(regs['deltas'], list) and np.isscalar(regs['deltas']):
+        regs['deltas'] = [regs['deltas']]
     if not isinstance(up_bounds, list):
         up_bounds = [up_bounds]
 
-    K = C + alpha*(2*np.eye(N)-np.ones((N,N)))
+    K = C + regs['alpha']*(2*np.eye(N)-np.ones((N,N)))
     L = np.linalg.pinv(C, rcond=1e-6, hermitian=True)
+
+    L = np.where(L > 0, 0, L)
+    L[np.eye(N, dtype=bool)] = -1*np.sum(L,1)
+
     prev_L = L
     w = utils.L_inv_op(L)
     lambdas, V = np.linalg.eigh(L)
@@ -163,9 +172,7 @@ def SGL_show_conv(C, g_funcs, up_bounds, cs, regs, A_true,
     lamb_prev = lambdas
     A_n = np.linalg.norm(A_true, 'fro')**2
     A_true_n = A_true/np.linalg.norm(A_true, 'fro')
-
     As = np.zeros((N, N, max_iters+1))
-
     As[:,:,0] = np.diag(np.diag(L)) - L
 
     errA = np.zeros(max_iters)
@@ -178,42 +185,29 @@ def SGL_show_conv(C, g_funcs, up_bounds, cs, regs, A_true,
     conv_c_n = np.zeros((max_iters,len(g_funcs)))
     for t in range(max_iters):
         # Step 1: closed form solution
-        z = utils.Lstar_op(V@np.diag(lambdas)@V.T-1/beta*K)
+        z = utils.Lstar_op(V@np.diag(lambdas)@V.T-1/regs['beta']*K)
         gradient = utils.Lstar_op(utils.L_op(w))-z
         w = np.maximum(0, w-gradient/(2*N))
         L = utils.L_op(w)
+        
+        if np.all((w == 0)):
+            print('WARNING: L_hat is 0. Returning L from previous iteration')
+            return prev_L, lamb_prev
 
         # Step 2: eigendecomposition of step 1
         _, V = np.linalg.eigh(L)
 
-        # Step 3: solved with cvx
         d = np.diag(V.T@L@V)[1:]
-        lambdas_hat = cp.Variable(N-1)  # Fisrt eigenvalue is 0
-        contraints = []
-        for i, g_func in enumerate(g_funcs):
-            expr = g_func(lambdas_hat, N)
-            if expr.curvature == "AFFINE":
-                contraints.append((expr - cs[i])**2 <= deltas[i])
-            elif expr.curvature == "CONCAVE":
-                contraints.append((expr - cs[i]) >= -deltas[i])
-            else:
-                contraints.append((expr - cs[i]) <= deltas[i])
-
-        up_bounds_obj = 0
-        for up_bound in up_bounds:
-            up_bounds_obj += gamma*up_bound(lambdas_hat, lambdas[1:], N)
-
-
-        obj = cp.Minimize(cp.sum(-cp.log(lambdas_hat)) \
-            + beta/2*cp.sum_squares(lambdas_hat-d) + up_bounds_obj)
-
-        prob = cp.Problem(obj, contraints)        
-        prob.solve()
-        lambdas = np.insert(lambdas_hat.value,0,0)  # Prepend eigenvalue 0 at the begining
+        lambdas, prob_status = step3(d, lambdas, g_funcs, up_bounds, cs, regs)
         
+        if prob_status in ['infeasible', 'unbounded', 'infeasible_inaccurate']:
+            print('WARNING: problem status', prob_status)
+            return prev_L, lamb_prev
+
+
         sim_Ls = np.linalg.norm(L-V@np.diag(lambdas)@V.T,'fro')
         opt_vals[t] = -np.log(np.prod(lambdas[1:])) + np.trace(K@L) \
-                 + beta/2*sim_Ls
+                 + regs['beta']/2*sim_Ls
 
         L_conv = np.linalg.norm(L-prev_L, 'fro')**2/np.linalg.norm(prev_L, 'fro')**2
         lambd_conv = np.linalg.norm(lambdas-lamb_prev, 2)**2/np.linalg.norm(lamb_prev, 2)**2
@@ -221,7 +215,7 @@ def SGL_show_conv(C, g_funcs, up_bounds, cs, regs, A_true,
         if verbose:
             sim_Ls = np.linalg.norm(L-V@np.diag(lambdas)@V.T,'fro')
             print('{}. status: {} - L_cnv: {:.6f} - Lam_cnv: {:.3f} - L-VLambdaL\': {:.3f}'.
-                format(t, prob.status, L_conv, lambd_conv, sim_Ls))
+                format(t, prob_status, L_conv, lambd_conv, sim_Ls))
             
             for i, g_func in enumerate(g_funcs):
                 c_aux =  g_func(lambdas, N).value
@@ -236,14 +230,12 @@ def SGL_show_conv(C, g_funcs, up_bounds, cs, regs, A_true,
 
         errA2[t] = np.linalg.norm(A_true_n-As[:,:,t+1]/np.linalg.norm(As[:,:,t+1],'fro'))**2
 
-        conv_A[t] = np.linalg.norm(As[:,:,t+1]-As[:,:,t], 'fro')**2/np.linalg.norm(As[:,:,t], 'fro')**2
-        conv_lamb[t] = np.linalg.norm(lambdas-lamb_prev,2)**2/np.linalg.norm(lamb_prev, 2)**2
+        conv_A[t] = np.linalg.norm(As[:,:,t+1]-As[:,:,t], 'fro')/np.linalg.norm(As[:,:,t], 'fro')
+        conv_lamb[t] = np.linalg.norm(lambdas-lamb_prev,2)/np.linalg.norm(lamb_prev, 2)
 
 
         conv_lamb[t] = np.linalg.norm(lambdas-lamb_prev, 2)**2/np.linalg.norm(lamb_prev, 2)**2
-
-        if L_conv < epsilon:
-            print('CONVERGENCE AT ITERATION:', t)
-            epsilon = -1
+        lamb_prev = lambdas
+        
 
     return As, opt_vals, sims_Ls, errA, errA2, conv_A, conv_lamb, conv_c_n
